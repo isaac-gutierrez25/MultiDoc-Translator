159faacf61dc50aa7fabecf6f6e405dbb9cc0fd7
isaac-gutierrez25 <isaacgutierrezchoquer@gmail.com>
2025-10-31 19:42:02 -0300

    Update README.md

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.carrusel-principal').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<img src="img/Arrow_left.svg">',
+    nextArrow: '<img src="img/Arrow_right.svg">',
+  });
+
+  $('.carrusel-secundario').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<img src="img/Arrow_left.svg">',
+    nextArrow: '<img src="img/Arrow_right.svg">',
+  });
